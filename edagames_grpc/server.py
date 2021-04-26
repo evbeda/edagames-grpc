319a7355af93ad55c19ae71af589acd46826f27b
Andres Bonilla <abonilla@eventbrite.com>
2021-04-26 16:46:41 -0300

    Fix grpc filenames

--- edagames_grpc/server.py
+++ edagames_grpc/server.py
@@ -1,9 +1,9 @@
 import grpc
-import edagames_pb2
-import edagames_pb2_grpc
+import eda_games_pb2
+import eda_games_pb2_grpc
 
 
-class EdaGamesService(edagames_pb2_grpc.TestServiceServicer):
+class EdaGamesService(eda_games_pb2_grpc.EdaGameServiceServicer):
     pass
 
 
@@ -11,7 +11,7 @@ class EdaGamesGRPC:
     def __init__(self, bind_ip: str = '0.0.0.0', port: int = 50001):
         listen_addr = f'{bind_ip}:{port}'
         self.server = grpc.aio.server()
-        edagames_pb2_grpc.add_EdaGamesServiceServicer_to_server(
+        eda_games_pb2_grpc.add_EdaGameServiceServicer_to_server(
             EdaGamesService(),
             self.server
         )
