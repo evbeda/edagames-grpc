04141504d9d69686d5013015fe39089a436e9372
Andres Bonilla <abonilla@eventbrite.com>
2021-04-26 17:43:35 -0300

    Implement gRPC conversions

--- edagames_grpc/server.py
+++ edagames_grpc/server.py
@@ -6,30 +6,44 @@ from typing import Dict
 
 
 class EdaGamesService(eda_games_pb2_grpc.EdaGameServiceServicer):
+    def __init__(self, delegate):
+        super().__init__()
+        self.delegate = delegate
+
     async def CreateGame(
         self,
         request: eda_games_pb2.CreateGameRequest,
         context: grpc.aio.ServicerContext,
-    ) -> eda_games_pb2.CreateGameResponse:
-        pass
+    ) -> eda_games_pb2.Idgame:
+        game_id = self.delegate.create_game(request.players)
+        return eda_games_pb2.Idgame(idgame=game_id)
 
     async def ExecuteAction(
         self,
-        request: eda_games_pb2.CreateGameRequest,
+        request: eda_games_pb2.Idgame,
         context: grpc.aio.ServicerContext,
     ) -> eda_games_pb2.GameStateResponse:
-        pass
+        game_state = self.delegate.execute_action(
+            request.idgame,
+            request.data,
+        )
+        response = eda_games_pb2.GameStateResponse()
+        response.current_player = game_state.current_player
+        response.next_player = game_state.next_player
+        response.turn_data.update(game_state.turn_data)
+        response.play_data.update(game_state.play_data)
+        return response
 
     async def EndGame(
         self,
-        request: eda_games_pb2.CreateGameRequest,
+        request: eda_games_pb2.Idgame,
         context: grpc.aio.ServicerContext,
     ) -> eda_games_pb2.GameStateResponse:
-        pass
+
 
     async def Penalize(
         self,
-        request: eda_games_pb2.CreateGameRequest,
+        request: eda_games_pb2.Idgame,
         context: grpc.aio.ServicerContext,
     ) -> eda_games_pb2.GameStateResponse:
         pass
@@ -40,7 +54,7 @@ class EdaGamesGRPC:
         listen_addr = f'{bind_ip}:{port}'
         self.server = grpc.aio.server()
         eda_games_pb2_grpc.add_EdaGameServiceServicer_to_server(
-            EdaGamesService(),
+            EdaGamesService(self),
             self.server,
         )
         self.server.add_insecure_port(listen_addr)
@@ -52,13 +66,13 @@ class EdaGamesGRPC:
         return await self.server.stop(timeout)
 
     async def create_game(players: list[str]) -> str:
-        pass
+        raise NotImplementedError
 
-    async def execute_action(game_id: str, game_data: Dict) -> GameState:
-        pass
+    async def execute_action(game_id: str, game_data: Dict[]) -> GameState:
+        raise NotImplementedError
 
     async def end_game(game_id: str) -> GameState:
-        pass
+        raise NotImplementedError
 
     async def penalize(game_id: str) -> GameState:
-        pass
+        raise NotImplementedError
