b8a32609a6e0adf542c5b798a1e8be8c50130350
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-04-27 10:28:25 -0300

    all services

--- edagames_grpc/client.py
+++ edagames_grpc/client.py
@@ -13,27 +13,44 @@ class ClientGRPC:
         stub = eda_games_pb2_grpc.EdaGameServiceStub(channel)
         return stub
 
+    async def Saludo(self):
+        response = self.stub.SayHello(
+            eda_games_pb2.HelloRequest(name='you')
+        )
+        return "Greeter client received: {}".format(response.message)
+
     async def CreateGame(self, players):
         response = self.stub.CreateGame(
             eda_games_pb2.CreateGameRequest(players=players)
         )
-        return "Echo the list of players: " + response.idgame
+        return "Echo the list of players: {}".format(response.idgame)
 
+    async def ExecuteAction(self, id):
+        response = self.stub.ExecuteAction(
+            eda_games_pb2.ExecuteActionRequest(idgame=id)
+        )
+        return "Echo execute action: {}".format(response.next_player)
 
-class ClientInterface:
-    def __init__(self):
-        self.grpc = ClientGRPC()
+    async def EndGame(self, id):
+        response = self.stub.EndGame(
+            eda_games_pb2.Idgame(idgame=id)
+        )
+        return "Echo endgame: {}".format(response.next_player)
 
-    async def create_game(self, players):
-        if type(players) == list:
-            return await self.grpc.CreateGame(players)
-        else:
-            return 'Pasame una list'
+    async def Penalize(self, id):
+        response = self.stub.Penalize(
+            eda_games_pb2.Idgame(idgame=id)
+        )
+        return "Echo penalize: {}".format(response.next_player)
 
 
 async def testing():
-    x = ClientInterface()
-    print(await x.create_game(['Juan', 'Marcos']))
+    x = ClientGRPC()
+    print(await x.Saludo())
+    print(await x.CreateGame(['Juan', 'Marcos']))
+    print(await x.ExecuteAction('123'))
+    print(await x.EndGame('123'))
+    print(await x.Penalize('123'))
 
 
 asyncio.run(testing())
