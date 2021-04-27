c85e720408ea76b0063b4a7e1fd81c39462d9aab
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-04-27 11:42:59 -0300

    class client working

--- edagames_grpc/client.py
+++ edagames_grpc/client.py
@@ -1,7 +1,7 @@
 import grpc
-import asyncio
 import eda_games_pb2
 import eda_games_pb2_grpc
+from google.protobuf.struct_pb2 import Struct
 
 
 class ClientGRPC:
@@ -13,44 +13,24 @@ class ClientGRPC:
         stub = eda_games_pb2_grpc.EdaGameServiceStub(channel)
         return stub
 
-    async def Saludo(self):
-        response = self.stub.SayHello(
-            eda_games_pb2.HelloRequest(name='you')
-        )
-        return "Greeter client received: {}".format(response.message)
-
     async def CreateGame(self, players):
-        response = self.stub.CreateGame(
+        return self.stub.CreateGame(
             eda_games_pb2.CreateGameRequest(players=players)
         )
-        return "Echo the list of players: {}".format(response.idgame)
 
-    async def ExecuteAction(self, id):
-        response = self.stub.ExecuteAction(
-            eda_games_pb2.ExecuteActionRequest(idgame=id)
+    async def ExecuteAction(self, id, data_dict):
+        data_struct = Struct()
+        data_struct.update(data_dict)
+        return self.stub.ExecuteAction(
+            eda_games_pb2.ExecuteActionRequest(idgame=id, data=data_struct)
         )
-        return "Echo execute action: {}".format(response.next_player)
 
     async def EndGame(self, id):
-        response = self.stub.EndGame(
+        return self.stub.EndGame(
             eda_games_pb2.Idgame(idgame=id)
         )
-        return "Echo endgame: {}".format(response.next_player)
 
     async def Penalize(self, id):
-        response = self.stub.Penalize(
+        return self.stub.Penalize(
             eda_games_pb2.Idgame(idgame=id)
         )
-        return "Echo penalize: {}".format(response.next_player)
-
-
-async def testing():
-    x = ClientGRPC()
-    print(await x.Saludo())
-    print(await x.CreateGame(['Juan', 'Marcos']))
-    print(await x.ExecuteAction('123'))
-    print(await x.EndGame('123'))
-    print(await x.Penalize('123'))
-
-
-asyncio.run(testing())
