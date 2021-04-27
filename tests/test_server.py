df017963a8be4612e355e6066fcf59fcb81a9109
Andres Bonilla <abonilla@eventbrite.com>
2021-04-27 10:23:14 -0300

    Standarize class names with client

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,12 +1,14 @@
 import unittest
 from unittest.mock import patch, AsyncMock
-from edagames_grpc.server import EdaGamesGRPC
+from edagames_grpc.server import ServerGRPC
+from edagames_grpc.server import ServerInterface
+import grpc_testing
 
 
-class TestGRPCServer(unittest.IsolatedAsyncioTestCase):
+class TestServerInterface(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self):
-        self.server = EdaGamesGRPC()
+        self.server = ServerInterface()
 
     async def test_start(self):
         server_patched = AsyncMock()
@@ -19,3 +21,22 @@ class TestGRPCServer(unittest.IsolatedAsyncioTestCase):
         self.server.server = server_patched
         await self.server.stop()
         server_patched.stop.assert_called()
+
+
+class TestServerGRPC(unittest.IsolatedAsyncioTestCase):
+
+    # def setUp(self):
+    #     grpc_patched = AsyncMock()
+    #     self.service = EdaGamesService(grpc_patched)
+
+    # async def test_create_game(self):
+    #     pass
+
+    def setUp(self):
+        self._real_time = grpc_testing.strict_real_time()
+        servicer = _server_application.FirstServiceServicer()
+        descriptors_to_servicers = {
+            _application_testing_common.FIRST_SERVICE: servicer
+        }
+        self._real_time_server = grpc_testing.server_from_dictionary(
+            descriptors_to_servicers, self._real_time)
