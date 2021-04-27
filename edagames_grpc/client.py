bdf052626d56851fbea80aa23e443dd21ebcabd6
Andres Bonilla <abonilla@eventbrite.com>
2021-04-27 15:44:14 -0300

    Convert GameStateResponse returns to GameState

--- edagames_grpc/client.py
+++ edagames_grpc/client.py
@@ -1,7 +1,10 @@
 import grpc
+from google.protobuf.struct_pb2 import Struct
+
+from game_state import GameState
+
 import eda_games_pb2
 import eda_games_pb2_grpc
-from google.protobuf.struct_pb2 import Struct
 
 
 class ClientGRPC:
@@ -18,15 +21,19 @@ class ClientGRPC:
     async def execute_action(self, game_id, data_dict):
         data_struct = Struct()
         data_struct.update(data_dict)
-        request = eda_games_pb2.ExecuteActionRequest(idgame=game_id, data=data_struct)
-        return await self.stub.ExecuteAction(request)
+        response = await self.stub.ExecuteAction(
+            eda_games_pb2.ExecuteActionRequest(idgame=game_id, data=data_struct)
+        )
+        return GameState.from_protobuf_game_state_response(response)
 
     async def end_game(self, game_id):
-        return await self.stub.EndGame(
+        response = await self.stub.EndGame(
             eda_games_pb2.Idgame(idgame=game_id)
         )
+        return GameState.from_protobuf_game_state_response(response)
 
     async def penalize(self, game_id):
-        return await self.stub.Penalize(
+        response = await self.stub.Penalize(
             eda_games_pb2.Idgame(idgame=game_id)
         )
+        return GameState.from_protobuf_game_state_response(response)
