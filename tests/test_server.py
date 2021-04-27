fb0e6805a0cf88c29f4ae0c8809c13b8b49b6a3c
Andres Bonilla <abonilla@eventbrite.com>
2021-04-27 11:27:13 -0300

    Add test for server-side create game gRPC call

--- tests/test_server.py
+++ tests/test_server.py
@@ -3,6 +3,8 @@ from unittest.mock import patch, AsyncMock
 from edagames_grpc.server import ServerGRPC
 from edagames_grpc.server import ServerInterface
 import grpc_testing
+import grpc
+import eda_games_pb2
 
 
 class TestServerInterface(unittest.IsolatedAsyncioTestCase):
@@ -34,9 +36,28 @@ class TestServerGRPC(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self):
         self._real_time = grpc_testing.strict_real_time()
-        servicer = _server_application.FirstServiceServicer()
+        self.delegate = AsyncMock()
+        servicer = ServerGRPC(self.delegate)
+        self.service = eda_games_pb2.DESCRIPTOR.services_by_name['EdaGameService']
         descriptors_to_servicers = {
-            _application_testing_common.FIRST_SERVICE: servicer
+            self.service: servicer,
         }
-        self._real_time_server = grpc_testing.server_from_dictionary(
-            descriptors_to_servicers, self._real_time)
+        self.server = grpc_testing.server_from_dictionary(
+            descriptors_to_servicers, self._real_time,
+        )
+
+    async def test_create_game(self):
+        request = eda_games_pb2.CreateGameRequest()
+        request.players.extend(['Player 1', 'Player 2'])
+        self.delegate.create_game.return_value = '0001'
+        rpc = self.server.invoke_unary_unary(
+            self.service.methods_by_name['CreateGame'],
+            (),
+            request,
+            None,
+        )
+        response, trailing_metadata, code, details = rpc.termination()
+        response_awaited = await response
+        expected = eda_games_pb2.Idgame(idgame='0001')
+        self.assertEqual(response_awaited, expected)
+        self.assertIs(code, grpc.StatusCode.OK)
