f802521020b3250e2bf89213043e3f9836c7035f
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-04-28 11:36:50 -0300

    game_state rename

--- edagames_grpc/game_state.py
+++ edagames_grpc/game_state.py
@@ -4,27 +4,27 @@ import eda_games_pb2
 
 
 class GameState:
+    previous_player: str
     current_player: str
-    next_player: str
     turn_data: Dict
     play_data: Dict
 
     def __init__(
         self,
+        previous_player: str,
         current_player: str,
-        next_player: str,
         turn_data: Dict,
         play_data: Dict,
     ):
+        self.previous_player = previous_player
         self.current_player = current_player
-        self.next_player = next_player
         self.turn_data = turn_data
         self.play_data = play_data
 
     def to_protobuf_struct(self) -> eda_games_pb2.GameStateResponse:
         response = eda_games_pb2.GameStateResponse()
+        response.previous_player = self.previous_player
         response.current_player = self.current_player
-        response.next_player = self.next_player
         response.turn_data.update(self.turn_data)
         response.play_data.update(self.play_data)
         return response
@@ -34,8 +34,8 @@ class GameState:
         game_state_response: eda_games_pb2.GameStateResponse,
     ) -> 'GameState':
         return GameState(
+            game_state_response.previous_player,
             game_state_response.current_player,
-            game_state_response.next_player,
             game_state_response.turn_data,
             game_state_response.play_data
         )
