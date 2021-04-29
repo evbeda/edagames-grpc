2774e8622e2f55a4af7a1b5b4fd5da6466379752
Andres Bonilla <abonilla@eventbrite.com>
2021-04-29 14:58:07 -0300

    Migrate tests to pytest

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,40 +1,60 @@
-import grpc_testing
-import unittest
 from unittest.mock import AsyncMock
+import pytest
+
+from edagames_grpc.game_start import GameStart
+from edagames_grpc.utils import struct_to_dict
 
 from edagames_grpc.server import ServerGRPC
 from edagames_grpc.server import ServerInterface
 from edagames_grpc import eda_games_pb2
+from edagames_grpc import eda_games_pb2_grpc
 
 
-class TestServerInterface(unittest.IsolatedAsyncioTestCase):
-
-    def setUp(self):
-        self.server = ServerInterface()
-
-    async def test_start(self):
-        server_patched = AsyncMock()
-        self.server.server = server_patched
-        await self.server.start()
-        server_patched.start.assert_called()
-
-    async def test_stop(self):
-        server_patched = AsyncMock()
-        self.server.server = server_patched
-        await self.server.stop()
-        server_patched.stop.assert_called()
+# Test Server Interface
+@pytest.fixture
+def server_interface():
+    return ServerInterface()
 
 
-class TestServerGRPC(unittest.IsolatedAsyncioTestCase):
+@pytest.mark.asyncio
+async def test_server_start(server_interface):
+    server_patched = AsyncMock()
+    server_interface.server = server_patched
+    await server_interface.start()
+    server_patched.start.assert_called()
 
-    def setUp(self):
-        self._real_time = grpc_testing.strict_real_time()
-        self.delegate = AsyncMock()
-        servicer = ServerGRPC(self.delegate)
-        self.service = eda_games_pb2.DESCRIPTOR.services_by_name['EdaGameService']
-        descriptors_to_servicers = {
-            self.service: servicer,
-        }
-        self.server = grpc_testing.server_from_dictionary(
-            descriptors_to_servicers, self._real_time,
-        )
+
+@pytest.mark.asyncio
+async def test_server_stop(server_interface):
+    server_patched = AsyncMock()
+    server_interface.server = server_patched
+    await server_interface.stop()
+    server_patched.stop.assert_called()
+
+
+# Test gRPC
+@pytest.fixture(scope='module')
+def grpc_add_to_server():
+    return eda_games_pb2_grpc.add_EdaGameServiceServicer_to_server
+
+
+@pytest.fixture(scope='module')
+def grpc_servicer():
+    delegate = AsyncMock()
+    delegate.create_game.return_value = GameStart('0001', 'Player 1', {})
+    return ServerGRPC(delegate)
+
+
+@pytest.fixture(scope='module')
+def grpc_stub_cls(grpc_channel):
+    return eda_games_pb2_grpc.EdaGameServiceStub
+
+
+@pytest.mark.asyncio
+async def test_create_game(grpc_stub):
+    request = eda_games_pb2.CreateGameRequest()
+    request.players.extend(['Player 1', 'Player 2'])
+    response = await grpc_stub.CreateGame(request)
+    assert response.idgame == '0001'
+    assert response.current_player == 'Player 1'
+    assert struct_to_dict(response.turn_data) == {}
