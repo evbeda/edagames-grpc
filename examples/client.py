c317774b449bb46880dd6d79b6104cfc1dabe318
vgatica-eb <vgatica@eventbrite.com>
2021-04-29 15:49:11 -0300

    update client example

--- examples/client.py
+++ examples/client.py
@@ -4,20 +4,21 @@ from edagames_grpc.client import ClientGRPC
 
 async def test_client():
     c = ClientGRPC(port=50051)
-    r = await c.create_game(['asd'])
+    r = await c.create_game(['elenzo', 'lajuli'])
     print(f'Received {r}')
     r = await c.execute_action(
-        '0000-0002',
+        r.game_id,
         {
-            'action': 'asd',
+            'action': 'move',
             'data': {
-                'clave': 'otro asd',
-                'otra clave': 'mas asdasd',
+                'from_row': 1,
+                'from_col': 2,
+                'to_rol': 1,
+                'to_col': 2,
             },
         },
     )
     print(f'Received {r}')
-
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
