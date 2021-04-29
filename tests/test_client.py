64b390c38535cb5192ba2aaeb245117e14bb4cbc
Andres Bonilla <abonilla@eventbrite.com>
2021-04-29 17:41:48 -0300

    Test remaining client methods

--- tests/test_client.py
+++ tests/test_client.py
@@ -15,3 +15,24 @@ def test_client():
 async def test_client_create_game(test_client):
     await test_client.create_game(['Player 1', 'Player 2'])
     test_client.stub.CreateGame.assert_called()
+
+
+@pytest.mark.asyncio
+async def test_client_execute_action(test_client):
+    await test_client.execute_action(
+        '0001',
+        {},
+    )
+    test_client.stub.ExecuteAction.assert_called()
+
+
+@pytest.mark.asyncio
+async def test_client_end_game(test_client):
+    await test_client.end_game('0001')
+    test_client.stub.EndGame.assert_called()
+
+
+@pytest.mark.asyncio
+async def test_client_penalize(test_client):
+    await test_client.penalize('0001')
+    test_client.stub.Penalize.assert_called()
