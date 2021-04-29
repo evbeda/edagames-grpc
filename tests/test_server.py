d7c1f48ab5023b5e7638c154c9cb7da91a280914
Andres Bonilla <abonilla@eventbrite.com>
2021-04-29 12:50:00 -0300

    Fix test running

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,44 +1,43 @@
-# import grpc_testing
-# import unittest
-# from src.edagames_grpc.probando import prueba_nada
-# from unittest.mock import AsyncMock
+import grpc_testing
+import unittest
+from unittest.mock import AsyncMock
 
-# from src.edagames_grpc.server import ServerGRPC
-# from src.edagames_grpc.server import ServerInterface
-# from src.edagames_grpc import eda_games_pb2
-
-
-# class TestServerInterface(unittest.IsolatedAsyncioTestCase):
-
-#     def setUp(self):
-#         self.server = ServerInterface()
-
-#     async def test_start(self):
-#         server_patched = AsyncMock()
-#         self.server.server = server_patched
-#         await self.server.start()
-#         server_patched.start.assert_called()
-
-#     async def test_stop(self):
-#         server_patched = AsyncMock()
-#         self.server.server = server_patched
-#         await self.server.stop()
-#         server_patched.stop.assert_called()
-
-#     def test_no_anda(self):
-#         self.assertEqual(1, 1)
+from edagames_grpc.server import ServerGRPC
+from edagames_grpc.server import ServerInterface
+from edagames_grpc import eda_games_pb2
 
 
-# class TestServerGRPC(unittest.IsolatedAsyncioTestCase):
+class TestServerInterface(unittest.IsolatedAsyncioTestCase):
 
-#     def setUp(self):
-#         self._real_time = grpc_testing.strict_real_time()
-#         self.delegate = AsyncMock()
-#         servicer = ServerGRPC(self.delegate)
-#         self.service = eda_games_pb2.DESCRIPTOR.services_by_name['EdaGameService']
-#         descriptors_to_servicers = {
-#             self.service: servicer,
-#         }
-#         self.server = grpc_testing.server_from_dictionary(
-#             descriptors_to_servicers, self._real_time,
-#         )
+    def setUp(self):
+        self.server = ServerInterface()
+
+    async def test_start(self):
+        server_patched = AsyncMock()
+        self.server.server = server_patched
+        await self.server.start()
+        server_patched.start.assert_called()
+
+    async def test_stop(self):
+        server_patched = AsyncMock()
+        self.server.server = server_patched
+        await self.server.stop()
+        server_patched.stop.assert_called()
+
+    def test_no_anda(self):
+        self.assertEqual(1, 1)
+
+
+class TestServerGRPC(unittest.IsolatedAsyncioTestCase):
+
+    def setUp(self):
+        self._real_time = grpc_testing.strict_real_time()
+        self.delegate = AsyncMock()
+        servicer = ServerGRPC(self.delegate)
+        self.service = eda_games_pb2.DESCRIPTOR.services_by_name['EdaGameService']
+        descriptors_to_servicers = {
+            self.service: servicer,
+        }
+        self.server = grpc_testing.server_from_dictionary(
+            descriptors_to_servicers, self._real_time,
+        )
