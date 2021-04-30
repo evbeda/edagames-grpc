51b5261d12e828c8b92e5f3e5060760bc31d03b7
Andres Bonilla <abonilla@eventbrite.com>
2021-04-30 12:01:10 -0300

    Add mock game and simple turn switching to test server

--- examples/server.py
+++ examples/server.py
@@ -2,6 +2,15 @@ import asyncio
 
 from edagames_grpc.server import ServerInterface
 from edagames_grpc.game_state import GameState
+from edagames_grpc.game_start import GameStart
+
+
+class TestGame:
+    def __init__(self, players):
+        self.players = players
+        self.current = 0
+        self.prev = None
+        self.instance = self
 
 
 class ServerTest(ServerInterface):
@@ -9,19 +18,25 @@ class ServerTest(ServerInterface):
     async def create_game(self, players):
         print(f'Received players: {players}')
 
+        TestGame(players)
         gid = '0000-0001'
         print(f'Returned game id: {gid}')
-        return gid
+        return GameStart(gid, 'uno', {})
 
     async def execute_action(self, game_id, game_data):
         print(f'Received game id: {game_id}')
         print(f'Received game data: {game_data}')
 
+        TestGame.instance.prev = TestGame.instance.current
+        TestGame.instance.current = (TestGame.instance.current + 1) % 2
+
+        game_data.update({'state': 'valid'})
+
         game_state = GameState(
-            'Player 1',
-            'Player 2',
+            f'Player {TestGame.instance.current + 1}',
+            f'Player {TestGame.instance.prev + 1}',
             {'board': 'xxxxxxxxxxxxxxxxxxx'},
-            {'action': 'move', 'data': {'row_from': 2, 'row_to': 3}},
+            {'action': 'move', 'data': game_data},
         )
         print(f'Returned game state: {game_state}')
         return game_state
